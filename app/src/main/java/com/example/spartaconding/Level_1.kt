0b10d1c012309a2a29b191d41f8f81c975826b45
yangyunhyeok <ikx1030@gmail.com>
2023-07-20 13:03:17 +0900

    lv2 까지

---
+++ app/src/main/java/com/example/spartaconding/Level_1.kt
@@ -0,0 +1,30 @@
+package com.example.spartaconding
+
+import kotlin.math.sign
+
+fun main(){
+    println("더하기, 빼기, 나누기, 곱하기 중 원하는 기호를 선택하세요")
+    println("반드시 +, -, *, / 중에 선택")
+    var usersign = readLine()!!.toString()
+    println("첫번째 수를 입력하세요")
+    var usernum1 = readLine()!!.toDouble()
+    println("두번째 수를 입력하세요")
+    var usernum2 = readLine()!!.toDouble()
+
+    var input = Lv1()
+    var calculator = input.lv1(usersign, usernum1, usernum2)
+    println(calculator)
+
+}
+
+    class Lv1 {
+        fun lv1(sign: String, num1: Double, num2: Double): Any {
+            return when (sign) {
+                "+" -> num1 + num2
+                "-" -> num1 - num2
+                "*" -> num1 * num2
+                "/" -> num1 / num2
+                else -> println("잘못된 연산자 입니다.")
+            }
+        }
+    }
